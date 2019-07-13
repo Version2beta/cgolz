3086c89ab70653c4a33a0e7b72e28acb8076d7b8
Rob Martin <rob@version2beta.com>
2019-07-13 16:44:27 -0600

    clean up

--- lib/cgolz/helpers.ex
+++ lib/cgolz/helpers.ex
@@ -5,23 +5,11 @@ defmodule Cgolz.Helpers do
   Generate a random grid for testing. The grid will go from -x to x, and -y to y.
   """
 
-  alias Cgolz.Sites
-
   @spec random_grid(range_x :: integer(), range_y :: integer()) :: Sites.state()
   def random_grid(range_x, range_y) do
-    for x <- (-1 * range_x)..range_x, y <- (-1 * range_y)..range_y do
-      Enum.random([false, false, false, true]) && {{x, y}, %{occupant: :zombie}}
+    for x <- 0..range_x, y <- 0..range_y do
+      Enum.random([false, false, false, true]) && {x, y}
     end
     |> Enum.filter(& &1)
-    |> Enum.into(%{})
   end
-
-  @doc """
-  random_cell(range_x, range_y)
-
-  Pick a cell within a given range at random.
-  """
-  @spec random_cell(range_x :: integer, range_y :: integer) :: {integer, integer}
-  def random_cell(range_x, range_y),
-    do: {:rand.uniform(2 * range_x) - range_x, :rand.uniform(2 * range_y) - range_y}
 end
